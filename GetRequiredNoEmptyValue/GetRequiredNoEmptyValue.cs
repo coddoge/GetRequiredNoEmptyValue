ce43d58b952ab6fa6487a77282c52c296611e706
Coder <coder@example.com>
2023-04-18 17:37:23 +0800

    publish

---
+++ GetRequiredNoEmptyValue/GetRequiredNoEmptyValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.Configuration;
+
+public static class ConfigurationExtensionMethodForGetRequiredNoEmptyValue
+{
+    public static string GetRequiredNoEmptyValue(this IConfiguration config, string jsonPath)
+    {
+        var value = config.GetValue<string>(jsonPath);
+
+        if (value == null || string.IsNullOrEmpty(value))
+        {
+            throw new InvalidOperationException($"Configuration value for '{jsonPath}' is required but not found or empty in the configuration.");
+        }
+
+        return value;
+    }
+}
+
